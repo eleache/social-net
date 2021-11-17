ba8b2e0ff6260e88a3c74c2c56e797a14bf7ed69
eleAche <89001242+eleache@users.noreply.github.com>
2021-11-17 01:16:19 +0000

    'init'

---
+++ src/index.js
@@ -0,0 +1,11 @@
+/*
+backend social-Network, author: eleAche
+
+
+
+*/ 
+
+const express = require('express');
+const config = require('./server/config');
+const app = config(express());
+app.listen(app.get('port'));
